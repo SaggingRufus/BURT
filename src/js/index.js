cf8a326e757133b0311ff83761566e41399d6a5d
SaggingRufus <saggingrufus@outlook.com>
2022-10-01 08:58:08 -0300

    adding first window

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const mainWindowJsonFIle = require('fs');
+
+let jsonData = mainWindowJsonFIle.readFileSync("./src/json/main.json");
+let mainWindowProperties = JSON.parse(jsonData);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: mainWindowProperties.width,
+    height: mainWindowProperties.height,
+    icon: "./burt_icon.png"
+  })
+  win.loadFile('src/html/index.html')
+}
+app.whenReady().then(() => {
+  createWindow()
+})
